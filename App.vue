c6d7456557897164e2b5eb12f13abbefbc1c3e83
AbrarRamadava <114666990+AbrarRamadava@users.noreply.github.com>
2024-10-26 03:51:34 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<script>
+import Kalkulator from './components/Kalkulator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Kalkulator,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Kalkulator />
+  </div>
+</template>
+
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ Kalkulator.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      bilangan1: 0,
+      bilangan2: 0,
+      hasil: 0,
+    };
+  },
+  methods: {
+    hitung() {
+      this.hasil = this.bilangan1 + this.bilangan2;
+    },
+  },
+};
+</script>
+
+<template>
+<div>
+    <h2>Kalkulator Sederhana</h2>
+    <input type="number" v-model.number="bilangan1" placeholder="Masukkan Bilangan 1" />
+    +
+    <input type="number" v-model.number="bilangan2" placeholder="Masukkan Bilangan 2" />
+    =
+    <input type="number" :value="hasil" readonly />
+    <br /><br />
+    <button @click="hitung">Hitung</button>
+  </div>
+</template>
+
+<style scoped>
+input {
+  margin: 5px;
+  width: 100px;
+}
+button {
+  margin-top: 10px;
+}
+</style>
